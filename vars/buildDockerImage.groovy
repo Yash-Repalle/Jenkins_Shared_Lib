c33c1770de28284b380dacd9d050208d0b959664
Yaswanth345 <yaswanthgopi918@gmail.com>
2023-09-12 09:53:21 +0530

    added git checkout

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,9 @@
+def call(String project, String tag, String user){
+
+
+    sh """
+        docker image build -t ${user}/${project} .
+        docker image tag ${user}/${project} ${user}/${project}:${tag}
+        docker image tag ${user}/${project} ${user}/${project}:latest
+    """
+}
